7d8b267eb867f36da42c5a4afa573256cb8d1b3d
NurPro <nurik-31@mail.ru>
2022-03-25 00:04:06 +0500

    portfolio

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const os = require('os')
+const app = express()
+
+const network = os.networkInterfaces()
+const host = network.wlp2s0[0].address
+
+const PORT = 3000
+
+app.use(express.static(path.join(__dirname, 'assets')))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.post('/messages', async (req, res) => {
+    res.status(200)
+    res.end()
+})
+
+app.get('*', (req, res) => {
+    res.end()
+})
+
+
+app.listen(PORT, () => console.log(`server runnig http://${host}:${PORT}`))
